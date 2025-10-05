e02f87d5661a45a2771bb183e736cef67779aac2
Maksim Shylau <shilaums@gmail.com>
2025-10-05 20:24:08 +0200

    feat: use env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,15 +9,24 @@ import { OrdersModule } from './modules/orders/orders.module';
 import { CommonModule } from './common/common.module';
 import { Product } from './entities/product.entity';
 import { User } from './entities/user.entity';
+import configuration from './config/configuration';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'shop.db',
-      entities: [Product, User],
-      synchronize: true, // Use migrations in production
-      logging: false,
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'sqlite',
+        database: 'shop.db',
+        entities: [Product, User],
+        synchronize: configService.get<string>('nodeEnv') !== 'production', // Use migrations in production
+        logging: configService.get<string>('nodeEnv') === 'development',
+      }),
+      inject: [ConfigService],
     }),
     ProductsModule,
     AuthModule,
