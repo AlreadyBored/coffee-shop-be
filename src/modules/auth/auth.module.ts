506d2efc760896c4af932a49b8302dbde836013e
Maksim Shylau <shilaums@gmail.com>
2025-10-05 17:27:00 +0200

    chore: get rid of unnecessary comment

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { JwtStrategy } from './jwt.strategy';
     TypeOrmModule.forFeature([User]),
     PassportModule,
     JwtModule.register({
-      secret: process.env.JWT_SECRET || 'your-secret-key', // В продакшене использовать переменную окружения
+      secret: process.env.JWT_SECRET || 'your-secret-key',
       signOptions: { expiresIn: '24h' },
     }),
   ],
