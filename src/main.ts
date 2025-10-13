229fac50e39dfc2a2f966feee292ef95b0c412cb
Maksim Shylau <34455330+AlreadyBored@users.noreply.github.com>
2025-10-13 17:39:17 +0200

    feat: allow any requests (#9)

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,15 @@ import { AppModule } from './app.module';
 import { SeedService } from './common/services/seed.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: true,
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD'],
+      allowedHeaders: '*',
+      exposedHeaders: '*',
+    },
+  });
   const configService = app.get(ConfigService);
 
   const port = configService.get<number>('port');
